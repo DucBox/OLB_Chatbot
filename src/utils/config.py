0929ff31b860c5ae59ef7a96e649bbebba458f4f
Ngo Quang Duc <ngoquangduc@192.168.1.6.non-exists.ptr.local>
2025-04-20 21:15:46 +0700

    Change Token Limit Input to 64000

--- src/utils/config.py
+++ src/utils/config.py
@@ -2,7 +2,6 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 
-
 # === Base project root ===
 BASE_DIR = Path(__file__).resolve().parents[2]  # chatbot_rag/
 
@@ -13,11 +12,10 @@ LOG_FILE_XML = DATA_PATH / "history" / "chat_history.xml"
 CHROMADB_PATH = str(DATA_PATH / "chroma_db")
 
 FIREBASE_COLLECTION_NAME = "olb_embeddings"
-TOKEN_LIMIT = 2000 
+TOKEN_LIMIT = 64000 
 KEEP_LAST_N_PAIRS = 1
 CHUNK_SIZE = 700
 
-# API KEY LOADED
 load_dotenv()
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 if not OPENAI_API_KEY:
